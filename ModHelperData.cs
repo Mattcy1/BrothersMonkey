9b2b3653bdbee5f2ec151cb85a3b919fbf1fcc3f
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2025-02-02 02:00:49 +0100

    Im adding code rn its taking forever

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace BrotherMonkey;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "46.0";
+    public const string Version = "1.0.0";
+    public const string Name = "BrotherMonkey";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = "";
+    public const string RepoName = "";
+}
