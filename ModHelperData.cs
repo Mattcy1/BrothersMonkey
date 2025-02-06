1d53d502ac7b45fd10c9c9c3d07b43864a4c2ad9
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2025-02-06 10:47:10 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "46.0";
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Name = "BrotherMonkey";
 
     public const string Description = "An empty mod";
